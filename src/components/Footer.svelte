75ecf2f2daa1b2c5e152c8598f5ce5206d4548dd
rectifyoncesolutions-stack <rectifyoncesolutions@gmail.com>
2025-11-08 11:05:10 +0100

    Push it

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,6 @@
 <div class="flex w-full flex-col items-center justify-center border-t border-zinc-700 py-8 px-4">
   <div class="flex items-center">
-    Deployed on <a class="font-bold" href="https://vercel.com/home"
+    Deployed on <a class="font-bold" href="ROS"
       ><img src="ROS" class="h-5 ml-1" /></a
     >
   </div>
